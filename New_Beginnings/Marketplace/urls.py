cfa7cd60300aa7ba0d04c5ef78bf17b41613e60b
joecatarata <joecatarata@gmail.com>
2017-07-28 19:29:31 +0800

    Added design

--- New_Beginnings/Marketplace/urls.py
+++ New_Beginnings/Marketplace/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'Marketplace'
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^$', views.HomeView, name='home'),
+    url(r'^profile/(?P<user_id>[0-9]+)/$', views.ProfileView, name='profile'),
+    url(r'^item/(?P<user_id>[0-9]+)/$', views.ItemDetailsView, name='item'),
 ]
--- New_Beginnings/Marketplace/views.py
+++ New_Beginnings/Marketplace/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from django.views import generic
-from .models import Item
+from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 # Create your views here.
 
 
-class HomeView(generic.ListView):
+def HomeView(request):
+    all_items_list = Item.objects.all()
     template_name = 'Marketplace/home.html'
     context_object_name = 'all_items'
+    paginator = Paginator(all_items_list, 10)  # Show 25 contacts per page
 
-    def get_queryset(self):
-        return Item.objects.all()
+    page = request.GET.get('page')
+    try:
+        all_items = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        all_items = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        all_items = paginator.page(paginator.num_pages)
+
+    return render(request, 'Marketplace/home.html', {'all_items':all_items})
+
+def ProfileView(request, user_id):
+    user = get_object_or_404(Profile, pk = user_id)
+    items = list(Item.objects.filter(profile = user_id))
+
+    return render(request, 'Marketplace/profile.html', {'user': user, 'items':items})
+
+def ItemDetailsView(request, user_id):
+    item = get_object_or_404(Item, pk=user_id)
+    return render(request, 'Marketplace/details.html', {'item': item})
